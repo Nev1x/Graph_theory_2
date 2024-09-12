9746ba95a8278dc1c753d9d9d3c9c8de4f247d41
Nev1x <kirll4047@gmail.com>
2024-09-12 15:09:45 +0300

    “first”

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+class Vector {
+private:
+    int x;
+    int y;
+    int z;
+
+
+};
+
+int main () {
+
+
+
+
+    return 0;
+}
